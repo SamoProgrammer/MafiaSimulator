e52f6cf3bc7f525eafe54187d7283969fa1361fb
comrade-sami <samantoor.king@gmail.com>
2023-12-01 20:54:49 +0330

    Investor Controller and navmesh Updated

--- Assets/Code/Objects/Character.cs
+++ Assets/Code/Objects/Character.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 
-public class Character
+public class Character : MonoBehaviour
 {
     public int health = 100;
     public int money = 0;
-    
+
     public void OnCharacterDeath()
     {
 
