6e0c198711449cba60afb3f1a634078ddc5c6730
comrade-sami <samantoor.king@gmail.com>
2023-12-03 16:54:26 +0330

    thief Logic

--- Assets/Code/Objects/Characters/Thief.cs
+++ Assets/Code/Objects/Characters/Thief.cs
@@ -2,11 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class Thief : Character
 {
-    public void StealMoney(Character character){
-        character.money -= 5;
+
+    [SerializeField] int stealAmount = 2;
+    [SerializeField] GameObject workplaces;
+    Building[] availableBuildings;
+    float pickTimer = 0;
+    float stealTimer = 0;
+    Building targetBuilding;
+
+    private void Start()
+    {
+        availableBuildings = workplaces.GetComponentsInChildren<Building>();
+        PickTarget();
+
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        pickTimer += Time.deltaTime;
+        stealTimer += Time.deltaTime;
+        if (pickTimer > 60)
+        {
+            pickTimer = 0;
+            PickTarget();
+
+        }
+        if (this.transform.position == targetBuilding.transform.position)
+        {
+            if (stealTimer > 5)
+            {
+                StealMoney(targetBuilding);
+            }
+        }
+    }
+
+
+    void PickTarget()
+    {
+        var random = new System.Random();
+        targetBuilding = availableBuildings[random.Next(availableBuildings.Length)];
+        characterDestination = targetBuilding.buildingPosition;
+
+    }
+    public void StealMoney(Building building)
+    {
+        building.money -= stealAmount;
     }
 }
