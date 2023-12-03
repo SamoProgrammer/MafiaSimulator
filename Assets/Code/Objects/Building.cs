ec5a47236655ffb72a105fd27d835fb6d081e62f
SamoProgrammer <samo092813463@gmail.com>
2023-12-03 21:13:37 +0330

    inherting mono behaviour

--- Assets/Code/Objects/Building.cs
+++ Assets/Code/Objects/Building.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine;
 
 
-    public class Building
+public class Building:MonoBehaviour
     {
         public int money;
         
