b106324cb7df62255c32a80ca1ab09b5566fb667
comrade-sami <samantoor.king@gmail.com>
2023-12-05 15:19:11 +0330

    UI Menus

---
+++ Assets/Art/UI/Icons/MenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject uiMenu;
+    [SerializeField] GameObject startMenu;
+}
