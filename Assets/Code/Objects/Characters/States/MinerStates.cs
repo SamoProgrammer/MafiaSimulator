baebb9f81e00c223a92b06e6314a81fc8f143bdc
SamoProgrammer <samo092813463@gmail.com>
2023-12-05 15:22:36 +0330

    move to workplace fix

--- Assets/Code/Objects/Characters/States/MinerStates.cs
+++ Assets/Code/Objects/Characters/States/MinerStates.cs
@@ -2,5 +2,5 @@
 public enum MinerStates
 {
   
-    Mining
+    Mining,
 }
--- Assets/Code/Objects/Characters/Worker.cs
+++ Assets/Code/Objects/Characters/Worker.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 public class Worker : Character
 {
     private float time;
-    private List<House> housesToWork;
-    private House houseToWork;
+    private float pickTimer;
+    private bool hasAHouseToWork = false;
+    [SerializeField] GameObject workplaces;
+    private List<Building> housesToWork;
+    private Building houseToWork;
     public WorkerStates workerState = WorkerStates.GoingToHouseForWork;
 
     protected override void Start()
     {
         base.Start();
-        housesToWork = FindObjectsOfType<House>().ToList();
+        housesToWork = GetChildObjects(workplaces);
+
     }
 
     protected override void Update()
@@ -23,6 +27,21 @@ public class Worker : Character
 
     }
 
+    List<Building> GetChildObjects(GameObject parent)
+    {
+        // GetComponentsInChildren also includes the parent itself, so we filter it out
+        Transform[] childTransforms = parent.GetComponentsInChildren<Transform>(true);
+
+        // Convert Transform array to GameObject array
+        Building[] childObjects = new Building[childTransforms.Length - 1];
+        for (int i = 1; i < childTransforms.Length; i++)
+        {
+            childObjects[i - 1] = childTransforms[i].gameObject.GetComponent<Building>();
+        }
+
+        return childObjects.ToList();
+    }
+
     public void Work()
     {
         if (health == 100)
@@ -30,23 +49,34 @@ public class Worker : Character
             if (workerState == WorkerStates.Working)
             {
                 time += Time.deltaTime;
+                print((int)time);
                 if ((int)time == 20)
                 {
                     money += 20;
                     time = 0;
                     workerState = WorkerStates.GoingToHouseForWork;
                 }
+                else
+                {
+                    hasAHouseToWork = false;
+                }
             }
             else if (workerState == WorkerStates.GoingToHouseForWork)
             {
-                houseToWork = housesToWork[Random.Range(0, housesToWork.Count)];
-                Vector3 housePositionToWork = houseToWork.transform.position - new Vector3(-0.5f, 0, -0.5f);
-                characterDestination = housePositionToWork;
-                movementEnabled = true;
-                if (Vector3.Distance(transform.position, housePositionToWork) < 1.5f)
+                if (!hasAHouseToWork)
                 {
-                    workerState = WorkerStates.Working;
+
+                    houseToWork = housesToWork[Random.Range(0, housesToWork.Count)];
+                    hasAHouseToWork = true;
+                    Vector3 housePositionToWork = houseToWork.transform.position - new Vector3(-0.5f, 0, -0.5f);
+                    characterDestination = housePositionToWork;
+                    movementEnabled = true;
+                    if (Vector3.Distance(transform.position, housePositionToWork) < 1.5f)
+                    {
+                        workerState = WorkerStates.Working;
+                    }
                 }
+
             }
         }
 
